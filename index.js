84bc5346f0d48f101cb4a5a1e5a4612a69063305
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2021-08-21 23:25:33 -0500

    Fix Server

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const express = require('express');
-const app = express();
 const jsonServer = require('json-server');
-const path = require('path');
-const port = 3000
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
+server.use(middlewares);
+server.use(router);
 
-// ...
-
-// You may want to mount JSON Server on a specific end-point, for example /api
-// Optiona,l except if you want to have JSON Server default
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-app.use('/api', jsonServer.router('./db.json'));
-  
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+server.listen(port);

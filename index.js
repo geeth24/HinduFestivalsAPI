a2b226b35859334e9caa9d6b691ea1ad1a1d18b3
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2021-08-21 20:05:08 -0500

    Fix server

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var server = express();
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 server.use('/', jsonServer.defaults()); 
-const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-//server.use('/api', jsonServer.router('db.json'));
+server.use('/api', jsonServer.router('db.json'));
 
 server.listen(3000);

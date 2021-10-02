113214a501ca689cef2f247d39b99d35f0e75d58
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2021-10-02 00:14:33 -0500

    Add 10.2

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5050;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+
+server.exports = (req, res) => {
+    res.status(404).json({ message: 'Not Found' });
+  };
+
+  server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
